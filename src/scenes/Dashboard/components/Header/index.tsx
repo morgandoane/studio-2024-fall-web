4ccae432ca90460665a0b1640e6b3b399b5e2267
Morgan Doane <morganddoane@gmail.com>
2024-10-11 11:19:50 -0400

    tailwind

--- src/scenes/Dashboard/components/Header/index.tsx
+++ src/scenes/Dashboard/components/Header/index.tsx
@@ -2,36 +2,21 @@ import { FC } from 'react';
 
 const Header: FC = () => {
 	return (
-		<div
-			style={{
-				textAlign: 'center',
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-				position: 'relative',
-			}}
-		>
-			<div style={{ height: '16px' }} />
-			<h1
-				className="display"
-				style={{
-					whiteSpace: 'pre-line',
-				}}
-			>
+		<div className="flex flex-col text-center">
+			<div className="h-8" />
+			<h1 className="text-heading-2 whitespace-pre-line">
 				{`Korea Red Cross Events
 						and Their Impacts`}
 			</h1>
-			<div style={{ height: '16px' }} />
-			<h5
-				style={{
-					maxWidth: '800px',
-				}}
-			>
-				Discovering whether a policy was impactful is always a difficult task.
-				In this data visualization we attempt to visualize the policy and its
-				impact through a case study in Korea Red Cross.
-			</h5>
+			<div className="h-4" />
+			<div className="flex justify-center">
+				<p className="text-body-medium max-w-prose">
+					Discovering whether a policy was impactful is always a difficult task.
+					In this data visualization we attempt to visualize the policy and its
+					impact through a case study in Korea Red Cross.
+				</p>
+			</div>
+			<div className="h-4" />
 		</div>
 	);
 };
