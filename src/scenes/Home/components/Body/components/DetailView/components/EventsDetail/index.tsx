b8c704241fc263e13474a7560bb663174c5d982f
Hun Kim <hunkim98@gmail.com>
2024-10-16 01:33:51 -0400

    Add comment for morgan

--- src/scenes/Home/components/Body/components/DetailView/components/EventsDetail/index.tsx
+++ src/scenes/Home/components/Body/components/DetailView/components/EventsDetail/index.tsx
@@ -1,39 +1,48 @@
-import { Event } from '@data/events/Event';
-import { AnimatePresence, motion } from 'framer-motion';
-import { FC } from 'react';
-import EventCard from './components/EventCard';
+import { Event } from "@data/events/Event";
+import { AnimatePresence, motion } from "framer-motion";
+import { FC } from "react";
+import EventCard from "./components/EventCard";
 
 export interface EventsDetailProps {
-	events: Event[];
+  events: Event[];
 }
 
 const EventsDetail: FC<EventsDetailProps> = ({ events }) => {
-	return (
-		<div>
-			<div className="flex flex-col gap-8">
-				{events.map((event, i) => (
-					<EventCard key={i} event={event} index={i} />
-				))}
-				{
-					<AnimatePresence>
-						{events.length === 0 && (
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
-							>
-								<div className="border rounded-md p-8">
-									<p className="text-body-medium">
-										No KRC events occurred this month.
-									</p>
-								</div>
-							</motion.div>
-						)}
-					</AnimatePresence>
-				}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div
+        className="flex flex-col gap-8 max-w-prose border rounded-md p-8"
+        style={
+          {
+            // TODO: @Morgan - Make the border color have the same color as the ratio gradient
+            // borderColor: events.length > 0 ? getGradientColor(events[0].ratio) : "black",
+          }
+        }
+      >
+        {events.map((event, i) => (
+          <EventCard key={i} event={event} index={i} />
+        ))}
+
+        {
+          <AnimatePresence>
+            {events.length === 0 && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                {/* <div className="border rounded-md p-8"> */}
+                <p className="text-body-medium opacity-50">
+                  No KRC events occurred this month.
+                </p>
+                {/* </div> */}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default EventsDetail;
