8392bcbf1cc302f02bbe53af18c143f69d6e2b16
Morgan Doane <morganddoane@gmail.com>
2024-10-16 12:23:33 -0400

    retitle

--- src/scenes/Home/components/Header/index.tsx
+++ src/scenes/Home/components/Header/index.tsx
@@ -6,14 +6,15 @@ const Header: FC = () => {
 			<div className="flex-1 max-w-screen-lg flex flex-col justify-center text-center items-center">
 				<div className="h-8" />
 				<h1 className="text-heading-1 whitespace-pre-line">
-					{`Korean Red Cross
-					Events & Their Impacts`}
+					{`Visualizing The Blood
+					System of South Korea`}
 				</h1>
 				<div className="h-2" />
 				<p className="max-w-prose text-body-medium">
-					Discovering whether a policy was impactful is always a difficult task.
-					In this data visualization we attempt to visualize the policy and its
-					impact through a case study in Korea Red Cross.
+					Visualize the delicate balance of blood supply and demand in South
+					Korea. Dive into key data on donations, transfusion requirements, and
+					the influence of Korean Red Cross events on maintaining this essential
+					system.
 				</p>
 				<div className="h-4" />
 			</div>
